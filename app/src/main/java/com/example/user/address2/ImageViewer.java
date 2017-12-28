e15d0ade30dd113a510585d26d8b5db51870b354
Lee Ju Been <ljb7977@gmail.com>
2017-12-28 12:08:28 +0900

    appbar added in imageview

--- app/src/main/java/com/example/user/address2/ImageViewer.java
+++ app/src/main/java/com/example/user/address2/ImageViewer.java
@@ -1,23 +1,32 @@
 package com.example.user.address2;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.drm.DrmStore;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 import java.io.File;
 import java.io.IOException;
 
 import static android.content.ContentValues.TAG;
 
-public class ImageViewer extends AppCompatActivity {
+public class ImageViewer extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener, View.OnClickListener {
+
+    ActionBar ab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +37,66 @@ public class ImageViewer extends AppCompatActivity {
         String name = path.substring(path.lastIndexOf("/")+1);
         Log.i("PATH", path);
 
+        ab = getSupportActionBar();
+        ab.setTitle(name);
+
         ImageView iv = findViewById(R.id.imageView);
+
+        iv.setOnClickListener(this);
 
         Bitmap b = LoadBitmap(path);
         iv.setImageBitmap(b);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getSupportActionBar().hide();
+            }
+        }, 1000);
     }
 
-    public static synchronized int GetExifOrientation(String path)
+    public void onClick(View v){
+        if(ab.isShowing())
+            ab.hide();
+        else
+            ab.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.imageview_appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_delete_button:
+                View menuItemView = findViewById(R.id.action_delete_button);
+                PopupMenu popupMenu = new PopupMenu(this, menuItemView);
+                popupMenu.inflate(R.menu.imageview_menu);
+                popupMenu.setOnMenuItemClickListener(this);
+                popupMenu.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
     {
+        switch(item.getItemId()){
+            case R.id.action_delete:
+                Log.i("DELETE", "delete menu clicked");
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static synchronized int GetExifOrientation(String path) {
         int degree = 0;
         ExifInterface exif = null;
 
@@ -65,8 +126,7 @@ public class ImageViewer extends AppCompatActivity {
         return degree;
     }
 
-    public static synchronized Bitmap GetRotatedBitmap(Bitmap bitmap, int degree)
-    {
+    public static synchronized Bitmap GetRotatedBitmap(Bitmap bitmap, int degree) {
         if(degree != 0 && bitmap != null){
             Matrix m = new Matrix();
             m.setRotate(degree, bitmap.getWidth()/2.0f, bitmap.getHeight()/2.0f);
@@ -84,8 +144,7 @@ public class ImageViewer extends AppCompatActivity {
         return bitmap;
     }
 
-    public synchronized Bitmap LoadBitmap(String path)
-    {
+    public synchronized Bitmap LoadBitmap(String path) {
         try{
             File file = new File(path);
             if(!file.exists()){
@@ -112,8 +171,8 @@ public class ImageViewer extends AppCompatActivity {
             return null;
         }
     }
-    public static synchronized Bitmap LoadThumbnail(String path, String origin_path)
-    {
+
+    public static synchronized Bitmap LoadThumbnail(String path, String origin_path) {
         Bitmap bitmap = BitmapFactory.decodeFile(path);
         int degree = GetExifOrientation(origin_path);
         return GetRotatedBitmap(bitmap, degree);
